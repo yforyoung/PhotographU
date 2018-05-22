88c9fa90480f3bb56e20d4598c57a61df2ef6066
yforyoung <yforyoung@163.com>
2018-05-22 15:55:17 +0800

    更改部分布局，加入动画效果

--- PhotograhpU/src/main/java/com/example/y/photographu/IntroduceActivity.java
+++ PhotograhpU/src/main/java/com/example/y/photographu/IntroduceActivity.java
@@ -1,13 +1,48 @@
 package com.example.y.photographu;
 
+import android.support.design.widget.AppBarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.y.photographu.adapter.HomeStyleAdapter;
 
 public class IntroduceActivity extends AppCompatActivity {
+
+    private CollapsingToolbarLayoutState state;
+    private enum CollapsingToolbarLayoutState{
+        EXPANDED,
+        COLLAPSED,
+        INTERNEDIATE
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_introduce);
+        RecyclerView recyclerView=findViewById(R.id.introduce_judge_list);
+        recyclerView.setLayoutManager(new GridLayoutManager(this,2));
+        recyclerView.setAdapter(new HomeStyleAdapter());
+        AppBarLayout appBarLayout=findViewById(R.id.introduce_app_bar_layout);
+
+        Toolbar toolbar=findViewById(R.id.introduce_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        TextView title=toolbar.findViewById(R.id.introduce_toolbar_title);
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.introduce_menu,menu);
+        return true;
+
+    }
+
 }
